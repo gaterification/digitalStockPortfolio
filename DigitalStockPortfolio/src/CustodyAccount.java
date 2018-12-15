0ee5981039383e45d80b88db0f9911ad9ad6df7c
mmichel2012 <michelmueller@outlook.com>
2018-12-15 13:14:22 +0100

    JobType All Upper Case
    CustodyAccount Method: buyShare, sellShare, defineLimitSell,
    defineLimitBuy

--- DigitalStockPortfolio/src/CustodyAccount.java
+++ DigitalStockPortfolio/src/CustodyAccount.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 
 public class CustodyAccount {
 	// class attributes
-	private static CustodyAccount custodyAccount;	// singleton
+	private static CustodyAccount custodyAccount; // singleton
 	private static int trxNumber = 0;
-	
+
 	// attributes
 	private int custodyAccountNumber;
 	private ArrayList<Share> shares;
 	private Account account;
 	private JobWorker jobWorker;
 	private StockExchange stockExchange;
-	
+
 	// construct
 	private CustodyAccount(Account account) {
 		this.custodyAccountNumber = trxNumber;
@@ -21,7 +21,7 @@ public class CustodyAccount {
 		this.stockExchange = new Nasdaq();
 		this.jobWorker = JobWorker.getJobWorker(this.stockExchange); // TODO: singleton
 	}
-	
+
 	// methods
 	public static CustodyAccount getCustodyAccount(Account account) {
 		if (custodyAccount == null) {
@@ -29,42 +29,46 @@ public class CustodyAccount {
 		}
 		return custodyAccount;
 	}
-		
+
 	public double calculateWinOrLoss() {
 		// TODO: implement
 		return 0.0;
 	}
-	
+
 	public void buyShare(String isinNo) {
-		// TODO: implement
+		Job job = new Job(isinNo, JobType.BUY);
+		jobWorker.addJob(job);
 	}
-	
+
 	public void sellShare(String isinNo) {
-		// TODO: implement
+		Job job = new Job(isinNo, JobType.SELL);
+		jobWorker.addJob(job);
 	}
-	
+
 	public void defineLimitSell(String isinNo, double limit) {
-		// TODO: implement
+		Job job = new Job(isinNo, JobType.SELL, limit);
+		jobWorker.addJob(job);
 	}
-	
+
 	public void defineLimitBuy(String isinNo, double limit) {
-		// TODO: implement
+		Job job = new Job(isinNo, JobType.BUY, limit);
+		jobWorker.addJob(job);
 	}
-	
+
 	public double getMarketPrice(String isinNo) {
 		// TODO: implement
 		return 0.0;
 	}
-	
+
 	// getters
 	public int getCustodyAccountNumber() {
 		return this.custodyAccountNumber;
 	}
-	
+
 	public ArrayList<Share> getShares() {
 		return this.shares;
 	}
-	
+
 	public Account getAccount() {
 		return this.account;
 	}
