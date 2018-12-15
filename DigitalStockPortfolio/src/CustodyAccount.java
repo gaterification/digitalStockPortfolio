92e5f3470c9e51d5237e47dac35006c65757d493
Fabian Biondi <fabianbiondi@10.192.101.197>
2018-12-15 15:36:32 +0100

    add calculateWinOrLoss

--- DigitalStockPortfolio/src/CustodyAccount.java
+++ DigitalStockPortfolio/src/CustodyAccount.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 
 public class CustodyAccount {
 	// class attributes
-	private static CustodyAccount custodyAccount;	// singleton
+	private static CustodyAccount custodyAccount; // singleton
 	private static int trxNumber = 0;
-	
+
 	// attributes
 	private int custodyAccountNumber;
 	private ArrayList<Share> shares;
 	private Account account;
 	private JobWorker jobWorker;
 	private StockExchange stockExchange;
-	
+
 	// construct
 	private CustodyAccount(Account account) {
 		this.custodyAccountNumber = trxNumber;
@@ -21,7 +21,7 @@ public class CustodyAccount {
 		this.stockExchange = new Nasdaq();
 		this.jobWorker = JobWorker.getJobWorker(this.stockExchange); // TODO: singleton
 	}
-	
+
 	// methods
 	public static CustodyAccount getCustodyAccount(Account account) {
 		if (custodyAccount == null) {
@@ -29,48 +29,76 @@ public class CustodyAccount {
 		}
 		return custodyAccount;
 	}
-		
+
 	public double calculateWinOrLoss() {
-		// Arraylist mit vorhandenen Aktien aufrufen und Kaufwert zwischenspeichern, aktienkurs von vorhandenen aktien abrufen, 
-		// aktienkus aktuell und kaufprais verlgeichen, Neuer Wert - alter Wert = WinOrLoss
-		return 0.0;
+		// variabel für den End winOrLoss, wird beim durchschlaufen der arraylist
+		// angepasst
+
+		if (this.shares.size() > 0) {
+
+			double winOrLoss = 0.0;
+
+			// schlaufe um die ganze arraylist durchzugehen
+			for (Share currentShare : this.shares) {
+				// Variabel um die IsinNo des aktuellen Shares auszulesen und zwischenspeichern.
+				// Wird gebraucht um den aktuellen Wert danach abzufragen.
+				String shareIsinNo = currentShare.getIsinNo();
+				// Variabel um den alten Preis des aktuellen Shares auszulesn und
+				// zwischenspeichern
+				double oldPrice = currentShare.getCostPrice();
+				// Variabel um den aktuellen Preis auszulesn und zwischenspeichern
+				double actuallPrice = this.stockExchange.getMarketPrice(shareIsinNo);
+				// Variabel um den aktuellen und den alten Preis zu vergleichen und ausrechnen
+				double calculate = actuallPrice - oldPrice;
+
+				winOrLoss = winOrLoss + calculate;
+			}
+
+			// WinOrLoss
+			return 0.0;
+		} else {
+			System.out.println("Keine Aktien vorhanden");
+		}
 	}
-	
+
 	public void buyShare(String isinNo) {
 		Job job = new Job(isinNo, JobType.BUY);
 		this.jobWorker.addJob(job);
 	}
-	
+
 	public void sellShare(String isinNo) {
 		Job job = new Job(isinNo, JobType.SELL);
 		this.jobWorker.addJob(job);
 	}
-	
+
 	public void defineLimitSell(String isinNo, double limit) {
 		Job job = new Job(isinNo, JobType.SELL, limit);
 		this.jobWorker.addJob(job);
 
 	}
-	
+
 	public void defineLimitBuy(String isinNo, double limit) {
 		Job job = new Job(isinNo, JobType.BUY, limit);
 		this.jobWorker.addJob(job);
 	}
-	
+
 	public double getMarketPrice(String isinNo) {
+		// TODO: noetig? --> Kann mit this.stockExchange.getMarketPrice() gemacht werden
+		// --> noetig falls Abfrage von MarketPrice aus main gebraucht wird, dann
+		// braucht die main kein Zugriff auf den StockExchange
 		// TODO: implement
 		return 0.0;
 	}
-	
+
 	// getters
 	public int getCustodyAccountNumber() {
 		return this.custodyAccountNumber;
 	}
-	
+
 	public ArrayList<Share> getShares() {
 		return this.shares;
 	}
-	
+
 	public Account getAccount() {
 		return this.account;
 	}
