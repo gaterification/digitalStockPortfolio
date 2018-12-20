1d38107b05f26f544b702fe7bc16ddcad6f4aac2
mmichel2012 <michelmueller@outlook.com>
2018-12-20 16:24:15 +0100

    Method for Nasdaq

--- DigitalStockPortfolio/src/Nasdaq.java
+++ DigitalStockPortfolio/src/Nasdaq.java
@@ -1,26 +1,51 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
 // import java.io.IOException;
 
 public class Nasdaq extends StockExchange {
-	/*public void getMarketPriceTest(String test) throws IOException {
-		Stock s = YahooFinance.get(test);
-		System.out.print(s);
-	}*/
+	
+	  public void getMarketPriceTest(String test) throws IOException {
+		  Stock s = YahooFinance.get(test); 
+		  System.out.print(s); 
+	  }
+	 
+
+	Nasdaq() {
+		
+	}
 
 	@Override
-	public Share buyShare(String isinNo, double price) {
-		// TODO Auto-generated method stub
+	public Share buyShare(String isinNo, double money) throws IOException {
+		// TODO add custom Exception
+		if(money == getMarketPrice(isinNo)){
+			String name = YahooFinance.get(isinNo).getName();
+			Share s = new Share(name, money, isinNo);
+			return s;
+		}
+		
+
 		return null;
 	}
 
 	@Override
-	public double sellShare(Share share) {
-		// TODO Auto-generated method stub
-		return 0.0;
+	public double sellShare(Share share) throws IOException {
+		// TODO add custom Exception
+		Stock s = YahooFinance.get(share.getIsinNo());
+		BigDecimal b = s.getQuote().getPrice();
+		return b.doubleValue();
 	}
 
+		
+	
 	@Override
-	public double getMarketPrice(String isinNo) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double getMarketPrice(String isinNo) throws IOException {
+		// TODO add custom Exception
+		Stock s = YahooFinance.get(isinNo);
+		BigDecimal b = s.getQuote().getPrice();
+		return b.doubleValue();
 	}
 }
--- DigitalStockPortfolio/src/NasdaqTest.java
+++ DigitalStockPortfolio/src/NasdaqTest.java
@@ -1,15 +1,48 @@
-// import java.io.IOException;
+import java.io.IOException;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
 
 // TODO: remove legacy-Code --> this class is not used!
 public class NasdaqTest {
-	/*public static void main(String[] args) {
+	public static void main(String[] args) {
 		Nasdaq n = new Nasdaq();
+		Share s = null;
 		try {
-			n.getMarketPriceTest("TSLA");
+			double money = n.getMarketPrice("TSLA");
+			System.out.println("Preis TSLA: " + money);
+			try {
+				s = n.buyShare("TSLA", money);
+				System.out.println("Neuer Share: Name: " + s.getName() + ", isinNo: " + s.getIsinNo() + ", Price: " + s.getCostPrice());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			
+			
+			
 		}
-	}*/
+		
+		try {
+			double money = n.sellShare(s);
+			System.out.println("Geld zurück: " + money);
+			
 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+			
+			
+		}
+		
+	}
+
+//	Stock stock = YahooFinance.get("TSLA");
+//	Stock stock = new Stock("TSLA");
+	
 }
