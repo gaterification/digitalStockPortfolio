6f271f2a7d760d64bd19c5ac2e227fdd6cecc3d0
mmichel2012 <michelmueller@outlook.com>
2019-01-29 18:54:03 +0100

    cleanup

--- DigitalStockPortfolio/src/DigitalStockPortfolio/JobWorker.java
+++ DigitalStockPortfolio/src/DigitalStockPortfolio/JobWorker.java
@@ -8,8 +8,6 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class JobWorker {
-	// class attributes
-
 	// attributes
 	private ArrayList<Job> jobs;
 	private StockExchange stockExchange;
